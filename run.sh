269a3dfe8dd53ca2bd35d6a8256bbc1d75cb7259
Theodore <ted.kanell@gmail.com>
2025-10-25 17:45:14 -0600

    chore: update path

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-source ~/repos/MagicMirrorE-Ink/.venv/bin/activate
-~/repos/MagicMirrorE-Ink/.venv/bin/python ~/repos/MagicMirrorE-Ink/main.py -c "*/5 * * * *"
+source ~/repos/.venv/bin/activate
+~/repos/.venv/bin/python ~/repos/MagicMirrorE-Ink/main.py -c "*/5 * * * *"
