16a31a5ce32e4b4939cd12516b771e3df8876ebd
Theodore <ted.kanell@gmail.com>
2025-10-25 17:41:30 -0600

    chore: update run + service

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-source .venv/bin/activate
-python main.py -c "*/5 * * * *"
+source ~/repos/MagicMirrorE-Ink/.venv/bin/activate
+~/repos/MagicMirrorE-Ink/.venv/bin/python ~/repos/MagicMirrorE-Ink/main.py -c "*/5 * * * *"
